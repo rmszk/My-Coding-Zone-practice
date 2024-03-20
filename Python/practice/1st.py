0cf289b0213102fba7802d5d656673b812c45fc0
Rao Muhammad Shahroz Khan <113264705+rmszk@users.noreply.github.com>
2024-03-20 18:19:51 +0000

    practice code using function

---
+++ Python/practice/1st.py
@@ -0,0 +1,7 @@
+print("Calculator")
+
+num1 = int(input("Enter the 1st number:"))
+optr = input("Which operator calculate number..?")
+num2 = int(input("Enter the 2nd number:"))
+
+if ()
---
+++ Python/practice/Function.py
@@ -0,0 +1,27 @@
+#------ using string Function endswith-------# 
+print("#------ using string function endswith-----#")
+str =  "i am A python Developer"
+print(str.endswith("per")) #Developer Last word "per" for use function "endswith" /Answer/= True
+print(str.endswith("lop")) #Developer Last word "lop" for use function "endswith" /Answer/= False
+
+#------ using string Function capitalize 1st Char-------#
+print("#------using string function capitalize 1st char-------#")
+str1 = "i am a python developer"
+print(str1.capitalize()) # 1st Char capitalize "I"
+print(str1)
+
+#------ using string Function replace-------#
+print("#------ using string Function replace-------#")
+str2 = "i am full stack developer"
+print(str2.replace("full stack", "java"))
+print(str2) # replaces all occurence of old
+
+#------ using string Function Find-------#
+print("#------ using string Function Find-------#")
+str3 = "hello Rao Muhammad shahroz khan...!"
+print(str3.find("z")) # return 1st index of occurrer
+
+#------ using string Function count -------#
+print("#------ using string Function count-------#")
+str4 = "i am from studying python from iqra university"
+print(str4.count("i")) #counts the occurence of substr
