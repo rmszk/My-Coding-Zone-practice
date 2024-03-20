7a09a509b0ef87cf1b1cbd346a4e2cefe2f490f6
Rao Muhammad Shahroz Khan <113264705+rmszk@users.noreply.github.com>
2024-03-20 21:12:59 +0000

    practice on 21-march-2024 02 : 25 AM

---
+++ Python/practice/conditional statements/ifelse.py
@@ -0,0 +1,89 @@
+# trafic = input("which On the light..?")
+# light = trafic
+
+# if (light == "Red"):
+#     print("Stop")
+
+# elif(light == "Yellow"):
+#     print("Look")
+
+# elif(light == "Green"):
+#     print("Go")
+
+# else:
+#     print("Light is broken")
+
+
+
+
+"Grade Students based on marks"
+# marks = int(input("what is your Obtain Marks?"))
+
+# if(marks >= 90):
+#     print("Grade = 'A'")
+
+# elif(90 > marks >= 80):
+#     print("Grade, = 'B'")
+
+# elif(80 > marks >= 70):
+#     print("Grade, = 'C'")
+
+# elif(70 > marks >= 60):
+#     print("Grade, = 'D'")
+
+# else:
+#     print("Grade, = 'F'")
+
+
+# marks = int(input("Enter Student Marks :"))
+
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >= 80 and marks < 90):
+#     grade = "B"
+# elif(marks >= 70 and marks < 80):
+#     grade = "C"
+# elif(marks >= 60 and marks < 70):
+#     grade = "D"
+# else:
+#     grade = "F"
+# print("Grade of the student -->",grade)
+
+
+'Lets Practice '
+'WAP to check if number entered by the user is odd and even.'
+
+# num = int(input("enter the number :"))
+# if(num%2 == 0):
+#     print("This is even number :",num)
+# else:
+#     print("This is odd Number :",num)
+
+
+'WAP to find the greatest of 3 numbers entered by the user'
+
+num1 = int(input("enter a 1st Number A :"))
+num2 = int(input("enter a 2nd Number B :"))
+num3 = int(input("enter a 3rd Number C :"))
+
+if(num1 > num2):
+    print("A grater then B")
+elif(num1 > num3):
+    print("A grater then C")
+elif(num1 > num2 and num1 > num3):
+    print("A grater then B and C")
+elif(num2 > num1 ):
+    print("B grater then A")
+elif(num2 > num3):
+    print("B grater then C")
+elif(num2 > num1 and num2 > num3):
+    print("B grater then A and C ")
+elif(num3 > num1 ):
+    print("C grater then A")
+elif(num3 > num2):
+    print("C grater then B")
+elif(num3 > num1 and num3 > num2):
+    print("C grater then A and B")
+
+
+
