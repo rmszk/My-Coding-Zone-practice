94ac1a94096d90f2ed49f91d8bf84bca0f4227c9
Rao Muhammad Shahroz Khan <113264705+rmszk@users.noreply.github.com>
2024-03-27 21:32:29 +0000

    28-march-2K24 Loop practices

---
+++ Python/practice/Looping/1st.py
@@ -0,0 +1,64 @@
+# i = 1
+# while i  <= 10:
+#     print("Rao Muhammad shahroz Khan",i)
+#     i+=1
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i +=1
+
+# print("Loop Ended")
+
+
+#-----Question 1 table------#
+# n = 5
+# i = 1
+# while i <= 10:
+#     print(n,"x",i,"=",n*i)
+#     i +=1
+
+#------Question 2 table------#
+
+# table_No = int(input("Enter the Number for Table : "))
+# len = int(input("Enter the table lenght for print : "))
+# x = 1
+# while x <= len:
+#     multiple = table_No * x
+#     print(table_No,"X",x,"=",multiple)
+#     x +=1
+
+
+# #---Question: print the elements of the following list a loop
+# num = [1, 4, 9, 16, 17, 20, 22, 25, 36, 49, 64, 81, 100]
+# lenght = len(num)-1
+# i = 0
+# while i <= lenght:
+#     print(num[i])
+#     i +=1
+
+
+# Search for a number X in this tuple using loop:
+srch = int(input("Enter a number for your searching in the tuple : "))
+num1 = (1, 4, 9, 16, 17, 20, 22, 25, 36, 49, 64, 81, 100)
+x = 0
+while x < len(num1):
+    if(num1[x] == srch):
+     print("this number exist in this tuple index number ",x)
+    else:
+     print("this number not exist in the tuple")
+    #print(num1[x])
+    x += 1
+ 
+
+
+
+    
+
+
+
+
+
+
+
+    
