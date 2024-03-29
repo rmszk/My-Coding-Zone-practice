10f4d41eee0a84989f5d331ce7f044d7c2bbefdb
Rao Muhammad Shahroz Khan <113264705+rmszk@users.noreply.github.com>
2024-03-29 19:55:32 +0000

    30-03-2024

---
+++ Python/practice/Function/1st.py
@@ -0,0 +1,28 @@
+###WAP find the lenght of a list.(using is the parameter)
+# cities = ["Karachi", "Kotri", "Hyderabad", "Tando Adam", "Shahdadpur", "Nawabsha", "Lundo"]
+# district = ["Karachi", "Hyderabad", "Jamshoro", "Mirpurkhas", "Sanghar"]
+
+# def print_len(len(list)):
+#      print(list)
+# print_len(len(cities))
+# print_len(len(district))
+
+###WAP to print the elements if a list in a single line.(List is the parameter)
+# def printList(list):
+#      for i in list:
+#          (i, end=" ")
+# printList(district)
+
+
+###WAP to find the factorial of n.(n is the parameter)
+
+def fact (n):
+    x =1
+    for i in range(1,n+1):
+        x *= i
+    print(x,end = " ")
+        #i += 1
+fact(8)
+
+###WAP to convet USD to PAk
+
