46359b1f8811b3706ae9a0dc4eaf22623673f1ad
Rao Muhammad Shahroz Khan <113264705+rmszk@users.noreply.github.com>
2024-04-04 17:58:19 +0000

    Pending changes exported from your codespace

---
+++ Python/practice/Looping/2nd.py
@@ -0,0 +1,27 @@
+n = 2
+i = 2
+j = 10
+x = 2
+
+while(i <= 10):
+    print(f"---------------{i}------------------")
+    while(x <= 10):
+    # print(i)
+        print(f"{n} X {x} = {x*n}")
+        x += 1
+    n +=1
+    i +=1
+
+
+
+# for i in range(i,11):
+#     print(f"---------------{i}------------------")
+#     for x in range(1,11):
+#    # print(i)
+#        print(f"{n} X {x} = {x*n}")
+#        x += 1
+#     n += 1
+#     i += 1
+
+
+
