e10c171eed166d07e43a6aa97d7366784853b444
shahrozGit <rao.48539@iqra.edu.pk>
2024-04-19 01:23:46 +0500

    19-04-2024

---
+++ Python/practice/Looping/3rd.py
@@ -0,0 +1,33 @@
+# ####Q(1)
+# n = int(input("Enter the Number: "))
+# for i in range(1,n+1):
+#     print(i)
+#     i =+1
+
+# ###Q(2)
+# n = int(input("Enter the Number: "))
+# for i in range(n,0,-1):
+#     print(i)
+
+
+# ###Q(3)
+# n = int(input("Which number table you want:"))
+# for i in range(1,11):
+#     print(f"{n} X {i} = {n*i}")
+
+# ###(4)
+# n = int(input("Please Tell your number"))
+# sum = 0
+# for i in range(1,11):
+#     sum = sum + i
+#     print(f"sum = sum + {sum}")
+# print(f"sum = sum + {sum}")
+
+# ###(5)
+# num = int(input("Enter your factorial Number:"))
+# x = 1
+# for i in range(1,num+1):
+#     x*=i
+# print(x)
+
+###Q(6)
